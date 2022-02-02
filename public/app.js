4af9fb6bff223617a430f8eaaf230cbd3a53844a
Babis <63171080+b-a-b-i-s@users.noreply.github.com>
2022-02-02 02:25:05 +0200

    added connect,disconnect messages, enter for send, ctrl+enter for line break

--- public/app.js
+++ public/app.js
@@ -13,24 +13,45 @@ form.addEventListener("submit", function (e) {
 socket.on("chat message", function (msg) {
     var item = document.createElement("li");
     item.textContent = msg;
+    console.log("🚀 ~ file: app.js ~ line 16 ~ msg", msg)
+    
     messages.appendChild(item);
     window.scrollTo(0, document.body.scrollHeight);
 });
 
 
 
-// // Get the input field
-// var inputMessage = document.querySelector("#input-message");
+// Get the input field
+const inputMessage = document.querySelector("#input");
 
-// // Execute a function when the user releases a key on the keyboard
-// inputMessage.addEventListener("keyup", function (event) {
-//     // Number 13 is the "Enter" key on the keyboard
-//     if (event.keyCode === 13) {
-//         // Cancel the default action, if needed
-//         event.preventDefault();
-//         // Trigger the button element with a click
-//         document.querySelector("button").click();
-//         inputMessage.value = '';
-//     }
-// });
+let lock = false;
+
+// Execute a function when the user releases a key on the keyboard
+inputMessage.addEventListener("keydown", function (event) {
+    // Number 13 is the "Enter" key on the keyboard
+    console.log(event.keyCode)
+    if (event.keyCode === 13) {
+        if (lock){
+            inputMessage.value += "\n";
+
+        }
+        else {
+            // Cancel the default action, if needed
+            event.preventDefault();
+            // Trigger the button element with a click
+            document.querySelector("button").click();
+            inputMessage.value = '';   
+        }
+    }
+    if (event.keyCode === 17) {
+        lock = true;
+    }
+});
+
+inputMessage.addEventListener("keyup", function (event) {
+    if (event.keyCode === 17) {
+        lock = false;
+    }
+});
+
 
